0d72467807a0cd838573b808b6dd08c9a4ec2ebe
harshadeepg <harsha.iitbbs27@gmail.com>
2019-06-05 19:05:12 -0700

    Add files via upload

---
+++ TREC/GADGET/GADGET/data/remove_dir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -r albertaFloods2013
+rm -r guatemalaEarthquake2012
+rm -r queenslandFloods2013
+rm -r Attacks_features
+rm -r manilaFloods2013
+rm -r Tornado_features
+rm -r typhoonHagupit2014
+rm -r nepalEarthquake2015
+rm -r typhoonYolanda2013
+rm -r philipinnesFloods2012
+rm -r fire_features
+rm -r chileEarthquake2014
+rm -r Shooting_features
+rm -r Bombing_features
+rm -r italyEarthquakes2012
+
+
+
